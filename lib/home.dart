86cbfcb96e504a21374bae4c25b352da026224b0
LorenFiorini <lorenfiorini@gmail.com>
2021-07-26 09:45:36 -0700

    😄 Cards

--- lib/home.dart
+++ lib/home.dart
@@ -14,9 +14,107 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'model/products_repository.dart';
+import 'model/product.dart';
+
 
 class HomePage extends StatelessWidget {
+
   // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
+
+        child: Column(
+          // TODO: Center items on the card (103)
+
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
+                fit: BoxFit.scaleDown, //fitWidth
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+
+                    Text(
+                      product.name,
+                      style: theme.textTheme.headline6,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.subtitle2,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+  /*List<Card> _buildGridCards(int count) {
+    List<Card> cards= List.generate(
+        count,
+        (int index) => Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18 / 11, 
+                child: Image.asset('assets/diamond.png'),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(16, 12, 16, 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('title'),
+                    SizedBox(height: 8,),
+                    Text('Secondary text')
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+    return cards;
+  }*/
+
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -68,7 +166,8 @@ class HomePage extends StatelessWidget {
         crossAxisCount: 2,
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        children: <Widget>[
+        children: _buildGridCards(context),
+        /*children: <Widget>[
           Card(
             clipBehavior: Clip.antiAlias,
             child: Column(
@@ -92,7 +191,7 @@ class HomePage extends StatelessWidget {
               ],
             ),
           )
-        ],
+        ],*/ // Replaced by _buildGridCards(int)
       ),
 
       // TODO: Set resizeToAvoidBottomInset (101)
